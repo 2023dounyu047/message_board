68daf2428a80b8cd723f6efb6a46b197b7ab8175
User <User@DESKTOP-1RR8LM7>
2023-05-29 13:17:58 +0900

    UpdateServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -29,7 +29,7 @@ public class EditServlet extends HttpServlet {
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+     *      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em =DBUtil.createEntityManager();
 
@@ -41,6 +41,10 @@ public class EditServlet extends HttpServlet {
         //メッセージ情報とセッションIDをリクエストスコープに登録
         request.setAttribute("message",m);
         request.setAttribute("_token", request.getSession().getId());
+
+        //メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("message_id",m.getId());
+
 
         RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
